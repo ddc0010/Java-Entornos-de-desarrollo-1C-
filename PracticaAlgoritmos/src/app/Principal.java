c97ac95c3ed8d10e59ba2043a4eeb1e02be29261
ddc0010 <ddc0010@alu.medac.es>
2024-04-17 16:33:46 +0200

    Updated

---
+++ PracticaAlgoritmos/src/app/Principal.java
@@ -0,0 +1,19 @@
+package app;
+
+import util.Algoritmos;
+import java.util.Random;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		int [] num = new int [5];
+		Random random = new Random();
+		for(int i = 0; i < num.length;i++) {
+			num[i] = random.nextInt(10)+1;	}
+		
+		
+
+	}
+
+}
